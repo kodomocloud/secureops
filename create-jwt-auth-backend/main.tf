a59ef522585c02e789eb10044176509a7c6605c8
Ei Ei Khin <eieikhin@system76-pc.localdomain>
2024-09-30 01:33:31 +0630

    edit project name to Default Project in bound_claims

--- create-jwt-auth-backend/main.tf
+++ create-jwt-auth-backend/main.tf
@@ -84,7 +84,7 @@ resource "vault_jwt_auth_backend_role" "example" {
   bound_audiences = ["vault.workload.identity"]
   bound_claims_type = "glob"
   bound_claims = {
-    sub = "organization:eieikhin-org:project:secureops:workspace:*:run_phase:*"
+    sub = "organization:eieikhin-org:project:Default Project:workspace:*:run_phase:*"
   }
   user_claim = "terraform_full_workspace"
   role_type  = "jwt"
